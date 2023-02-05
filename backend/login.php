11ccddf1645b15a8741ba9e92a57953e61b7ff43
aziz-codes <azizisoffline@gmail.com>
2023-02-05 21:32:50 +0500

    toast setup

--- backend/login.php
+++ backend/login.php
@@ -1,6 +1,14 @@
 <?php
  include_once "connection.php";
 
-   echo "This is server response...";  
+   $username = mysqli_real_escape_string($con,$_POST['username']);
+   $password = mysqli_real_escape_string($con,$_POST['password']);
+
+   if(!empty($username) && !empty($password)){
+    echo "All good";
+   }
+   else{
+    echo "Both fields are required";
+   }
 
 ?>
